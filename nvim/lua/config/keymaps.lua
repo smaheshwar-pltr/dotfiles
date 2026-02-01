075fdb10c44ff9f054ddb84187a52e3e512404a8
Sreesh Maheshwar <smaheshwar@palantir.com>
2026-02-01 12:40:59 +0000

    Initial macOS keybinds

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,13 +7,28 @@
 
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename" })
 
--- Check if Neovim is running
-if vim.fn.has("nvim") == 1 then
-  -- Auto command for terminal open to map <Esc> to exit terminal mode
-  vim.api.nvim_create_autocmd("TermOpen", {
-    pattern = "*",
-    callback = function()
-      vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
-    end,
-  })
-end
+-- Window splits (matching tmux style with Ctrl instead of prefix)
+-- Ctrl+\ for vertical split, Ctrl+- for horizontal split
+vim.keymap.set("n", "<C-\\>", "<cmd>vsplit<cr>", { desc = "Vertical split" })
+vim.keymap.set("n", "<C-_>", "<cmd>split<cr>", { desc = "Horizontal split" }) -- Ctrl+- sends C-_
+
+-- Window resizing with Ctrl+Shift+hjkl (requires alacritty keybindings)
+-- These map to the escape sequences sent by alacritty
+vim.keymap.set("n", "<C-S-h>", "<cmd>vertical resize -5<cr>", { desc = "Resize window left" })
+vim.keymap.set("n", "<C-S-j>", "<cmd>resize -2<cr>", { desc = "Resize window down" })
+vim.keymap.set("n", "<C-S-k>", "<cmd>resize +2<cr>", { desc = "Resize window up" })
+vim.keymap.set("n", "<C-S-l>", "<cmd>vertical resize +5<cr>", { desc = "Resize window right" })
+
+-- NOTE: This interferes with LazyGit, so removing!
+-- Thank you, https://www.reddit.com/r/neovim/comments/1do3zgm/help_with_lazygit_plugin/
+
+-- -- Check if Neovim is running
+-- if vim.fn.has("nvim") == 1 then
+--   -- Auto command for terminal open to map <Esc> to exit terminal mode
+--   vim.api.nvim_create_autocmd("TermOpen", {
+--     pattern = "*",
+--     callback = function()
+--       vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
+--     end,
+--   })
+-- end
