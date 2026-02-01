d984ec25d439728b0629a09902963276cdf9c215
Sreesh Maheshwar <smaheshwar@palantir.com>
2026-02-01 14:27:49 +0000

    Add NeoVim window resizing

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,17 +7,41 @@
 
 vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename" })
 
--- Window splits (matching tmux style with Ctrl instead of prefix)
--- Ctrl+\ for vertical split, Ctrl+- for horizontal split
-vim.keymap.set("n", "<C-\\>", "<cmd>vsplit<cr>", { desc = "Vertical split" })
-vim.keymap.set("n", "<C-_>", "<cmd>split<cr>", { desc = "Horizontal split" }) -- Ctrl+- sends C-_
+-- Window splits (matching tmux style: Ctrl+w \ and Ctrl+w -)
+vim.keymap.set("n", "<C-w>\\", "<cmd>vsplit<cr>", { desc = "Vertical split" })
+vim.keymap.set("n", "<C-w>-", "<cmd>split<cr>", { desc = "Horizontal split" })
+
+-- Smart resize: moves the border in the direction of hjkl (like tmux)
+local function smart_resize(dir, amount)
+  if dir == "h" or dir == "l" then
+    local cur = vim.fn.winnr()
+    vim.cmd("wincmd l")
+    local is_rightmost = vim.fn.winnr() == cur
+    if not is_rightmost then vim.cmd("wincmd p") end
+
+    -- h=border left, l=border right
+    -- Rightmost: h=grow, l=shrink. Others: h=shrink, l=grow
+    local grow = (is_rightmost and dir == "h") or (not is_rightmost and dir == "l")
+    vim.cmd("vertical resize " .. (grow and "+" or "-") .. amount)
+  else
+    local cur = vim.fn.winnr()
+    vim.cmd("wincmd j")
+    local is_bottommost = vim.fn.winnr() == cur
+    if not is_bottommost then vim.cmd("wincmd p") end
+
+    -- j=border down, k=border up
+    -- Bottommost: k=grow, j=shrink. Others: j=grow, k=shrink
+    local grow = (is_bottommost and dir == "k") or (not is_bottommost and dir == "j")
+    vim.cmd("resize " .. (grow and "+" or "-") .. amount)
+  end
+end
 
 -- Window resizing with Ctrl+Shift+hjkl (requires alacritty keybindings)
--- These map to the escape sequences sent by alacritty
-vim.keymap.set("n", "<C-S-h>", "<cmd>vertical resize -5<cr>", { desc = "Resize window left" })
-vim.keymap.set("n", "<C-S-j>", "<cmd>resize -2<cr>", { desc = "Resize window down" })
-vim.keymap.set("n", "<C-S-k>", "<cmd>resize +2<cr>", { desc = "Resize window up" })
-vim.keymap.set("n", "<C-S-l>", "<cmd>vertical resize +5<cr>", { desc = "Resize window right" })
+-- Alacritty sends custom escape sequences: ESC _ R {H,J,K,L}
+vim.keymap.set("n", "\x1b_RH", function() smart_resize("h", 5) end, { desc = "Resize left" })
+vim.keymap.set("n", "\x1b_RL", function() smart_resize("l", 5) end, { desc = "Resize right" })
+vim.keymap.set("n", "\x1b_RJ", function() smart_resize("j", 2) end, { desc = "Resize down" })
+vim.keymap.set("n", "\x1b_RK", function() smart_resize("k", 2) end, { desc = "Resize up" })
 
 -- NOTE: This interferes with LazyGit, so removing!
 -- Thank you, https://www.reddit.com/r/neovim/comments/1do3zgm/help_with_lazygit_plugin/
